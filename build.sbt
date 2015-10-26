2c49bd515311b50e11d7f41c519e0ef971fff5e5
Jeff Hemminger <jeff@atware.co.jp>
2015-10-26 16:05:46 +0900

    dependency updates, scala update, quiet logging, etc

--- build.sbt
+++ build.sbt
@@ -3,8 +3,14 @@ version := "4.1-SNAPSHOT"
 
 licenses := Seq("Apache-2.0" -> url("http://opensource.org/licenses/Apache-2.0"))
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Dependencies.playSparkReact
+libraryDependencies ~= { _.map(_.exclude("org.slf4j", "slf4j-log4j12")) }
+dependencyOverrides ++= Set(
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.4.4"
+)
 
 enablePlugins(PlayScala)
+
+fork in run := true
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,39 +1,45 @@
 import sbt._
 
 object Version {
-  val spark          = "1.3.0"
-  val requirejs      = "2.1.17"
-  val requirejsText  = "2.0.10-3"
-  val react          = "0.13.3"
-  val jsxRequirejs   = "0.6.0"
-  val jquery         = "2.1.4"
-  val reactRouter    = "0.13.2"
-  val bootstrap      = "3.3.4"
-  val reactBootstrap = "0.19.1"
-  val scalaTest      = "2.2.1"
-  val scalaTestPlus  = "1.4.0-M3"
+  val spark = "1.5.1"
+  val requirejs = "2.1.20"
+  val requirejsText = "2.0.14"
+  val react = "0.14.0"
+  val jsxRequirejs = "0.6.0"
+  val jquery = "2.1.4"
+  val reactRouter = "0.13.2"
+  val bootstrap = "3.3.5"
+  val reactBootstrap = "0.25.1"
+  val scalaTest = "2.2.5"
+  val scalaTestPlus = "1.4.0-M4"
+  val slf4japi = "1.7.12"
+  val slf4jsimple = "1.7.12"
 }
 
 object Library {
-  val sparkCore     = "org.apache.spark"  %% "spark-core"                  % Version.spark
-  val sparkSql      = "org.apache.spark"  %% "spark-sql"                   % Version.spark
-  val requireJs     = "org.webjars"        % "requirejs"                   % Version.requirejs
-  val requireText   = "org.webjars"        % "requirejs-text"              % Version.requirejsText
-  val react         = "org.webjars"        % "react"                       % Version.react
-  val jsxRequire    = "org.webjars"        % "jsx-requirejs-plugin"        % Version.jsxRequirejs
-  val jquery        = "org.webjars"        % "jquery"                      % Version.jquery
-  val reactRouter   = "org.webjars"        % "react-router"                % Version.reactRouter
-  val bootstrap     = "org.webjars"        % "bootstrap"                   % Version.bootstrap
-  val reactBoot     = "org.webjars"        % "react-bootstrap"             % Version.reactBootstrap
-  val scalaTest     = "org.scalatest"     %% "scalatest"                   % Version.scalaTest
-  val scalaTestPlus = "org.scalatestplus" %% "play"                        % Version.scalaTestPlus
+  val sparkCore = "org.apache.spark" %% "spark-core" % Version.spark
+  val sparkSql = "org.apache.spark" %% "spark-sql" % Version.spark
+  val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest % "test"
+  val scalaTestPlus = "org.scalatestplus" %% "play" % Version.scalaTestPlus
+  val slf4japi = "org.slf4j" % "slf4j-api" % Version.slf4japi % "provided"
+  val slf4jnop = "org.slf4j" % "slf4j-nop" % Version.slf4jsimple % "test"
+
+  // webjar related dependencies
+  val requireJs = "org.webjars" % "requirejs" % Version.requirejs
+  val requireText = "org.webjars" % "requirejs-text" % Version.requirejsText
+  val react = "org.webjars" % "react" % Version.react
+  val jsxRequire = "org.webjars" % "jsx-requirejs-plugin" % Version.jsxRequirejs
+  val jquery = "org.webjars" % "jquery" % Version.jquery
+  val reactRouter = "org.webjars" % "react-router" % Version.reactRouter
+  val bootstrap = "org.webjars" % "bootstrap" % Version.bootstrap
+  val reactBoot = "org.webjars" % "react-bootstrap" % Version.reactBootstrap
 }
 
 object Dependencies {
 
   import Library._
 
-  val playSparkReact = List (
+  val playSparkReact = List(
     sparkCore,
     sparkSql,
     requireJs,
@@ -44,7 +50,7 @@ object Dependencies {
     reactRouter,
     bootstrap exclude("org.webjars", "jquery"),
     reactBoot,
-    scalaTest  % "test",
-    scalaTestPlus % "test"
+    scalaTest,
+    scalaTestPlus
   )
 }
